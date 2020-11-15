91189731397de30cf7291fc6e006d240acf1cd74
Nikita <nikitaodessafreelance@gmail.com>
2020-11-15 13:18:55 +0200

    update main function

--- onResize/js/onResize.js
+++ onResize/js/onResize.js
@@ -1,64 +1,64 @@
-class OnResize extends Code {
+class OnResize{
     constructor(options){
-        super({
-            size: options.size,
-            code: options.code,
-            onMinSize: (options.onMinSize == undefined) ? false: options.onMinSize,
-            heightResize: (options.heightResize == undefined) ? false: options.heightResize,
-        })
-        let isSize = false;
-        console.log(this.heightResize);
-        const mobileCode = () =>{
+        // options
+            this.size = options.size;
+            this.code = options.code;
+            this.onMinSize = (options.onMinSize == undefined) ? false: options.onMinSize;
+            this.heightResize = (options.heightResize == undefined) ? false: options.heightResize;
 
-            const minHeight = () => {
-                    if(this.size < window.innerHeight && isSize === false){
-                        this.code();
-                        isSize = true;
-                    }else if(this.size > window.innerHeight && isSize === true){
-                        isSize = false;
-                    }
-            };
-            const minWidth = () => {
-                if(this.size < window.innerWidth && isSize === false){
+        
+        let isSize = false;
+        // main function
+        const mobileCode = () =>{
+            // condition,condition2 - conditions in function showCode
+            const showCode = (condition,condition2) => {
+                if(condition){
                     this.code();
                     isSize = true;
-                }else if(this.size > window.innerWidth && isSize === true){
+                }else if(condition2){
                     isSize = false;
                 }
             };
-            const maxHeight = () => {
-                if(this.size > window.innerHeight && isSize === false){
-                    this.code();
-                    isSize = true;
-                }else if(this.size < window.innerHeight && isSize === true){
-                    isSize = false;
-                }
-            };
-            const maxWidth = () => {
-                if(this.size > window.innerWidth && isSize === false){
-                    this.code();
-                    isSize = true;
-                }else if(this.size < window.innerWidth && isSize === true){
-                    isSize = false;
-                }
-            };
+            // min
+            const minHeight = this.size < window.innerHeight && isSize === false;
+            const minHeight2 = this.size > window.innerHeight && isSize === true;
+            const minWidth = this.size < window.innerWidth && isSize === false;
+            const minWidtht2 = this.size > window.innerWidth && isSize === true;
+            // max
+            const maxHeight = this.size > window.innerHeight && isSize === false;
+            const maxHeight2 = this.size < window.innerHeight && isSize === true;
+            const maxWidth = this.size > window.innerWidth && isSize === false;
+            const maxWidth2 = this.size < window.innerWidth && isSize === true;
 
 
             if(this.onMinSize == true){
                 if(this.heightResize == true){
-                    minHeight();
+                    showCode(minHeight,minHeight2);
                 }else{
-                    minWidth();
+                    showCode(minWidth,minWidth2);
                 }
             }else{
                 if(this.heightResize == true){
-                    maxHeight();
+                    showCode(maxHeight,maxHeight2);
                 }else{
-                    maxWidth();
+                    showCode(maxWidth,maxWidth2);
                 }
             }
+            console.log('res');
+        }
+        const resize = () =>{
+            window.addEventListener("resize", mobileCode);
+        }
+        const removeResize = () =>{
+            window.removeEventListener("resize", mobileCode);
         }
         mobileCode();
-        window.addEventListener("resize", mobileCode);
     }
 }
+
+// const hero = new OnResize({
+//     size: 767,
+//     code: () => {
+//         document.querySelector('.hero').textContent = '767!!!';
+//     },
+// })
